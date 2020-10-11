979b2171ee223018ac81a1d1f637a42b25e4fc38
aymenla <aymen.laouini@ymail.com>
2020-10-11 17:45:36 +0200

    [IDC-001] updating supported SHA256 openSSL interfaces

--- idocrypto/include/idocrypto.h
+++ idocrypto/include/idocrypto.h
@@ -11,6 +11,10 @@
 #include <openssl/sha.h>
 
 /******************************* GLOBAL DEFINES ******************************/
+/* \brief size of hashed buffer, defined to 20 by OpenSSL in order to be compiant to 
+    US Federal Information Processing Standard FIPS PUB 180-4 (Secure Hash Standard), 
+    ANSI X9.30 */
+#define IDC_SHA_DIGEST_LENGTH   SHA_DIGEST_LENGTH
 
 /*********************** PRIVATE FUNCTION PROTOTYPES *************************/
 
@@ -26,6 +30,23 @@
  */
 int32_t idocrypto_sha256_init (SHA256_CTX *ctx);
 
-/*********************** PRIVATE FUNCTION DECLARATIONS ***********************/
+/*
+ *  \brief call to update a SHA256 Hash, to be called repetively as long as needed 
+ *  \param [in] ctx context to be given as input
+ *  \param [in] data pointer to data to be hashed
+ *  \param [in] len size of input data
+ * 
+ *  \return error code, see app_error_codes.h
+ */
+int32_t idocrypto_sha256_update (SHA256_CTX *ctx, const void *data, size_t len);
+
+/*
+ *  \brief call to obtain the final hash returned in pointer
+ *  \param [in] md pointer to hash, must be a valid pointer sized IDC_SHA_DIGEST_LENGTH
+ *  \param [in] ctx pointer to context structure
+ * 
+ *  \return error code, see app_error_codes.h
+ */
+int32_t idocrypto_SHA256_final (unsigned char *md, SHA256_CTX *ctx);
 
 #endif /* _IDOCRYPTO_H_ */
--- idocrypto/src/idocrypto.c
+++ idocrypto/src/idocrypto.c
@@ -3,6 +3,7 @@
 
 #include "app_error_codes.h"
 
+#include <openssl/sha.h>
 #include "idocrypto.h"
 
 
@@ -23,9 +24,15 @@ int32_t idocrypto_sha256_init (SHA256_CTX *ctx)
         printf("IDC_SHA256> [DBG] : initializing\n");
 
         sha256_ret = SHA256_Init(ctx);
-
-        // TODO : verify sha256_ret value 
-        app_ret = app_ok;
+        /* OpenSSL SHA interfaces return 1 if OK, 0 if error */
+        if(sha256_ret != 1) 
+        {
+            app_ret = app_ok;
+        }
+        else
+        {
+            app_ret = app_ressource_error;
+        }
     }
     else
     {
@@ -34,4 +41,57 @@ int32_t idocrypto_sha256_init (SHA256_CTX *ctx)
     
     return app_ok;
 }
+
+int32_t idocrypto_sha256_update (SHA256_CTX *ctx, const void *data, size_t len)
+{
+    enum app_error app_ret = app_error;
+    int32_t sha256_ret = 0;
+
+    if(ctx != NULL && data != NULL && len > (size_t)0)
+    {
+        sha256_ret = SHA256_Update (ctx, data, len);
+        /* OpenSSL SHA interfaces return 1 if OK, 0 if error */
+        if(sha256_ret != 1) 
+        {
+            app_ret = app_ok;
+        }
+        else
+        {
+            app_ret = app_ressource_error;
+        }
+    }
+    else
+    {
+        app_ret = app_param_error;
+    }
+
+    return app_ok;
+}
+
+int32_t idocrypto_sha256_final (unsigned char *md, SHA256_CTX *ctx)
+{
+    enum app_error app_ret = app_error;
+    int32_t sha256_ret = 0;
+
+    if(md != NULL && ctx != NULL)
+    {
+        sha256_ret = SHA256_Final (md, ctx);
+        /* OpenSSL SHA interfaces return 1 if OK, 0 if error */
+        if(sha256_ret != 1) 
+        {
+            app_ret = app_ok;
+        }
+        else
+        {
+            app_ret = app_ressource_error;
+        }
+    }
+    else
+    {
+        app_ret = app_param_error;
+    }
+
+    return app_ok;
+}
+
 /*********************** PRIVATE FUNCTION DECLARATIONS ***********************/
